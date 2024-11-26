e392d4244eb94e8bd01491e51885d1de127d8545
asa1984 <satoasa9913@gmail.com>
2024-11-26 17:14:27 +0900

    feat(kernel): 各処理の返り値をResult型に変更

--- kernel/src/model/virtual_machine/workflow.rs
+++ kernel/src/model/virtual_machine/workflow.rs
@@ -1,11 +1,26 @@
-use crate::model::virtual_machine::{
-    event::{CreateVirtualMachine, DeleteVirtualMachine, FindVirtualMachine, UpdateVirtualMachine},
-    VirtualMachine,
-};
-use crate::repository::{
-    proxmox_api::ProxmoxApiRepository, virtual_machine::VirtualMachineRepository,
+use crate::{
+    error::{DatabaseError, ProxmoxApiError, ValidationError},
+    model::virtual_machine::{
+        event::{
+            CreateVirtualMachine, DeleteVirtualMachine, FindVirtualMachine, UpdateVirtualMachine,
+        },
+        VirtualMachine,
+    },
+    repository::{proxmox_api::ProxmoxApiRepository, virtual_machine::VirtualMachineRepository},
 };
 use std::sync::Arc;
+
+#[derive(thiserror::Error, Debug)]
+pub enum VirtualMachineWorkflowError {
+    #[error("バリデーションに失敗しました: {0}")]
+    Validation(#[from] ValidationError),
+    #[error("DB処理実行時にエラーが発生しました: {0}")]
+    Database(#[from] DatabaseError),
+    #[error("Proxmox API呼び出し時にエラーが発生しました: {0}")]
+    ProxmoxApi(#[from] ProxmoxApiError),
+}
+
+pub type VirtualMachineWorkflowResult<T> = Result<T, VirtualMachineWorkflowError>;
 
 pub struct VirtualMachineWorkflow {
     db: Arc<dyn VirtualMachineRepository>,
@@ -22,37 +37,37 @@ impl VirtualMachineWorkflow {
 
     pub async fn create_virtual_machine(
         &self,
-        create_virtual_machine: CreateVirtualMachine,
-    ) -> VirtualMachine {
-        let virtual_machine = self.db.insert(create_virtual_machine).await;
-        self.proxmox_api.create_vm(virtual_machine.clone());
-        virtual_machine
+        event: CreateVirtualMachine,
+    ) -> VirtualMachineWorkflowResult<VirtualMachine> {
+        let vm = self.db.insert(event).await?;
+        let vm = self.proxmox_api.create_vm(vm).await?;
+        Ok(vm)
     }
 
     pub async fn delete_virtual_machine(
         &self,
-        delete_virtual_machine: DeleteVirtualMachine,
-    ) -> VirtualMachine {
-        let virtual_machine = self.db.delete(delete_virtual_machine).await;
-        self.proxmox_api.delete_vm(virtual_machine.clone());
-        virtual_machine
+        event: DeleteVirtualMachine,
+    ) -> VirtualMachineWorkflowResult<VirtualMachine> {
+        let vm = self.db.delete(event).await?;
+        let vm = self.proxmox_api.delete_vm(vm).await?;
+        Ok(vm)
     }
 
     pub async fn find_virtual_machine(
         &self,
-        find_virtual_machine: FindVirtualMachine,
-    ) -> VirtualMachine {
-        let virtual_machine = self.db.find(find_virtual_machine).await;
-        self.proxmox_api.fetch_vm(virtual_machine.clone()).await;
-        virtual_machine
+        event: FindVirtualMachine,
+    ) -> VirtualMachineWorkflowResult<VirtualMachine> {
+        let vm = self.db.find(event).await?;
+        let vm = self.proxmox_api.fetch_vm(vm).await?;
+        Ok(vm)
     }
 
     pub async fn update_virtual_machine(
         &self,
-        update_virtual_machine: UpdateVirtualMachine,
-    ) -> VirtualMachine {
-        let virtual_machine = self.db.update(update_virtual_machine).await;
-        self.proxmox_api.update_vm(virtual_machine.clone()).await;
-        virtual_machine
+        event: UpdateVirtualMachine,
+    ) -> VirtualMachineWorkflowResult<VirtualMachine> {
+        let vm = self.db.update(event).await?;
+        let vm = self.proxmox_api.update_vm(vm).await?;
+        Ok(vm)
     }
 }
