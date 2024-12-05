8c4de836a7437a8c592c7fc2e58f32ddc1513d57
rofen <734wh856@gmail.com>
2024-12-05 16:32:04 +0900

    feat: アンバリデーション構造体をCreateとUpdate向けに定義

--- kernel/src/model/virtual_machine/event.rs
+++ kernel/src/model/virtual_machine/event.rs
@@ -1,13 +1,16 @@
+use anyhow::anyhow;
+use std::convert::TryFrom;
 use uuid::Uuid;
 
+use crate::error::{ValidationError, ValidationResult};
+
+#[derive(Debug)]
 pub struct CreateVirtualMachine {
-    pub user_id: Uuid,
     pub template_id: Uuid,
     pub name: String,
 }
 
 pub struct DeleteVirtualMachine {
-    pub user_id: Uuid,
     pub virual_machine_id: Uuid,
 }
 
@@ -18,4 +21,57 @@ pub struct FindVirtualMachine {
 pub struct UpdateVirtualMachine {
     pub virtual_machine_id: Uuid,
     pub name: String,
+}
+
+#[derive(Debug)]
+pub struct UnvalidatedCreateVirtualMachine {
+    pub template_id: Uuid,
+    pub name: String,
+}
+
+pub struct UnvalidatedUpdateVirtualMachine {
+    pub virtual_machine_id: Uuid,
+    pub name: String,
+}
+
+impl TryFrom<UnvalidatedCreateVirtualMachine> for CreateVirtualMachine {
+    type Error = ValidationError;
+
+    fn try_from(value: UnvalidatedCreateVirtualMachine) -> ValidationResult<Self> {
+        if value.template_id.is_nil() {
+            return Err(ValidationError(
+                anyhow!("テンプレートIDが無効です").context("template_id is nil"),
+            ));
+        }
+        if value.name.trim().is_empty() {
+            return Err(ValidationError(
+                anyhow!("名前が1文字以上である必要があります").context("name is empty"),
+            ));
+        }
+        Ok(CreateVirtualMachine {
+            template_id: value.template_id,
+            name: value.name,
+        })
+    }
+}
+
+impl TryFrom<UnvalidatedUpdateVirtualMachine> for UpdateVirtualMachine {
+    type Error = ValidationError;
+
+    fn try_from(value: UnvalidatedUpdateVirtualMachine) -> ValidationResult<Self> {
+        if value.virtual_machine_id.is_nil() {
+            return Err(ValidationError(
+                anyhow!("テンプレートIDが無効です").context("template_id is nil"),
+            ));
+        }
+        if value.name.trim().is_empty() {
+            return Err(ValidationError(
+                anyhow!("名前が1文字以上である必要があります").context("name is empty"),
+            ));
+        }
+        Ok(UpdateVirtualMachine {
+            virtual_machine_id: value.virtual_machine_id,
+            name: value.name,
+        })
+    }
 }
